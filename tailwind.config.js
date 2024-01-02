738503996c1361d7845c60d42ac21a8b6aa153f6
eddiekeem <cm730712@gmail.com>
2024-01-03 01:03:09 +0900

    [✨Feat]: px을 rem으로 변환해주는 기능 추가
    - px을 작성하면 rem으로 값을 변환해주는 함수 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+import { range } from 'lodash';
+
+const pxToRem = (px, base = 16) => `${px / base}rem`;
+
+const pxToRemFunc = (start, end) =>
+  range(start, end).reduce((acc, px) => {
+    acc[`${px}pxr`] = pxToRem(px);
+    return acc;
+  }, {});
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.html",
-  ],
+  content: ['./index.html', './src/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        ...pxToRemFunc(0, 1000),
+      }, // px을 rem으로 변환
+      inset: {
+        ...pxToRemFunc(0, 1000),
+      },
+      fontSize: {
+        ...pxToRemFunc(0, 1000),
+      }, // px을 rem으로 변환
+      lineHeight: {
+        ...pxToRemFunc(0, 1000),
+      }, // px을 rem으로 변환
+      screens: {
+        mobile: '360px',
+        tablet: '768px',
+        desktop: '1280px',
+      },
+    },
   },
   plugins: [],
-}
-
+};
