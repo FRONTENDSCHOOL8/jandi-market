0bc892a375eb4840c6de5a3381536c7e51b6c001
SeungJun <deepvictory@naver.com>
2024-01-06 00:33:03 +0900

    [✨Feat] main 페이지 스와이퍼 초기단계 구현중
    - 스와이퍼 기능은 구현 완료
    - 커스터마이징 추가적으로 필요

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
+const swiper = new Swiper('.swiper', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
