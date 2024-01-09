121f19ffef0ae9e38f56d676eed7019f4a27a7fc
eddiekeem <cm730712@gmail.com>
2024-01-09 10:47:53 +0900

    [♻️Refactor]: 각 페이지 html 이름 index로 변경 및 헤더 수정

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        cart: resolve(__dirname, 'src/pages/cart/index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        register: resolve(__dirname, 'src/pages/register/index.html'),
+        productList: resolve(__dirname, 'src/pages/productList/index.html'),
+        productDetail: resolve(__dirname, 'src/pages/productDetail/index.html'),
       },
     },
   },
