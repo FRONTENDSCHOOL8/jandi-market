408591207cab2e5605b5e60da490afc4949ac074
eddiekeem <cm730712@gmail.com>
2024-01-09 11:30:59 +0900

    [🚚Rename]: 공통 컴포넌트 html 이름 index로 변경

--- src/js/include.js
+++ src/js/include.js
@@ -1,5 +1,5 @@
 // header.html 파일을 가져와서 header 요소에 삽입
-fetch('/src/components/header/header.html')
+fetch('/src/components/header/')
   .then((response) => response.text())
   .then((html) => {
     const headerElement = document.querySelector('#header');
@@ -11,7 +11,7 @@ fetch('/src/components/header/header.html')
   });
 
 // footer.html 파일을 가져와서 footer 요소에 삽입
-fetch('/src/components/footer/footer.html')
+fetch('/src/components/footer/')
   .then((response) => response.text())
   .then(async (html) => {
     const footerElement = document.querySelector('#footer');
