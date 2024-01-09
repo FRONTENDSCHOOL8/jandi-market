34ac33ae25fc316c6d58f367a7087a5d6742f308
eddiekeem <cm730712@gmail.com>
2024-01-10 00:14:02 +0900

    [✨Feat]: register 마크업 완성(일부 수정 필요)

---
+++ src/pages/register/register.js
@@ -0,0 +1,12 @@
+const addressFind = document.querySelector('#address_find');
+
+function findAddress() {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
+      // 예제를 참고하여 다양한 활용법을 확인해 보세요.
+    },
+  }).open();
+}
+
+addressFind.addEventListener('click', findAddress);
