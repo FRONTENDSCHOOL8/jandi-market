9129872f698213905b421cd049419a94584eef58
juhyuny <141771716+juhyuny@users.noreply.github.com>
2024-01-11 10:15:21 +0900

    cart 페이지 체크박스 구현 (#128)
    * [🎨Modify]: cart.hrml 상품 리스트 image, product_name a태그 링크 수정
    
    cart.html
    - 상품 리스트 image, product_name 클릭 시 detail 페이지로 이동
    
    * [🎨Modify]: cart.html h4 마크업 위치 변경
    
    cart.html
    - h4 마크업 위치 변경
    
    * [✨Feat]: cart.js cart list 전체선택 기능 구현
    
    cart.js
    - cart list 전체선택 기능 구현
    
    * [🔥Remove]: juhyuny 로컬feature/cart에 남은 cart.html 삭제(index.html 아님)
    
    juhyuny 로컬 feature/cart
    - 남아있던 cart.html 삭제(index.html 아님)
    
    * [🎨Modify]: include.js addCart if문 추가
    
    include.js
    - addCart if문 추가
    
    * [🔥Remove]: vite.config.js.~ 파일 삭제
    
    vite.config.js.~ 파일 삭제

---
+++ src/pages/cart/cart.js
@@ -0,0 +1,54 @@
+// cart list 전체선택 구현
+
+// 전체 체크박스
+const checkBoxes = document.querySelectorAll('input[name="cartList"]');
+// 선택된 체크박스
+// const selectedBoxes = document.querySelectorAll('input[name="cartList"]:checked');
+// select all 체크박스
+const itemSelectAll = document.querySelector('input[id="itemAllSelect"]');
+// select all 하단 체크박스
+const itemSelectAll2 = document.querySelector('input[id="itemAllSelect2"]');
+
+// 하나라도 풀리면 전체선택 풀리도록
+function unSelectAll(e)  {
+  
+  if(!e.target.checked){
+    itemSelectAll.checked = false;
+    itemSelectAll2.checked = false;
+  }
+  
+  let check = e.target.checked;
+  checkBoxes.forEach((checkbox) => {
+    if(checkbox.checked === false){
+      check = false;
+    }
+  })
+  if(check){
+    itemSelectAll.checked = true;
+    itemSelectAll2.checked = true;
+    
+  }
+  // 낱개가 모두 선택되면 전체선택 되도록
+}
+
+// 전체선택 체크하면 모두 체크되도록
+function selectAll(e)  {
+  
+  checkBoxes.forEach((checkbox) => {
+    checkbox.checked = e.target.checked
+  })
+  
+  console.log(e.target.checked);
+  
+}
+
+  
+checkBoxes.forEach((checkbox) => {
+  checkbox.addEventListener("click", unSelectAll)
+})
+
+itemSelectAll.addEventListener("click",selectAll);
+itemSelectAll2.addEventListener("click",selectAll);
+
+
+
