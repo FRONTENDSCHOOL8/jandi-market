f0f7946589595159cf1704f4dc876c3db72e2354
eddiekeem <cm730712@gmail.com>
2024-01-11 13:44:15 +0900

    [📑Docs]: .env 파일 생성

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,3 +1,14 @@
+const usersData = import.meta.env.VITE_PH_USERS;
+
+fetch(usersData)
+  .then((response) => {
+    if (response.ok === true) {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    data;
+  });
 const goToRegister = document.querySelector('#go_register');
 
 goToRegister.addEventListener('click', () => {
