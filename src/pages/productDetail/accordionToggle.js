ec10963df7d0f3219eb8e2087c3a6441d79f341b
bambiru <dj70083798@gmail.com>
2024-01-12 21:54:31 +0900

    [✨Feat]: 비밀글 안보이게
    - data-secret인 경우, 내용 안 보이게 만들었습니다.
      (추가 수정 예정)

--- src/pages/productDetail/accordionToggle.js
+++ src/pages/productDetail/accordionToggle.js
@@ -26,5 +26,45 @@ function toggleContent(titles, contents) {
     });
   });
 }
+
+/* -------------------------------------------------------------------------- */
+/*                                     비밀글                                  */
+/* -------------------------------------------------------------------------- */
+function secretCheck() {
+  inquiryTitle.forEach((title) => {
+    if (title.dataset.secret == 'true') {
+      let content = title.nextElementSibling;
+
+      title.innerHTML = `
+      <button class="flex font-semibold border-b border-b-gray-100">
+        <div class="text-gray-400 inquiry_table_subject"> 
+        비밀글입니다.
+          <span>
+          <svg width="12" height="14" aria-hidden="true">
+            <use href="/icon/_sprite.svg#Lock"></use>
+          </svg>
+        </span> 
+      </div>
+      <div class="table_width100">김*식</div>
+      <time datetime="2022-11-11" class="table_width100">
+        2022.11.11
+      </time>
+      <div class="text-center text-primary py-17pxr w-100pxr">
+        답변완료
+      </div>
+    </button>
+      `;
+
+      content.style.display = 'none';
+      content.setAttribute('aria-hidden', true);
+      content.hidden = true;
+      content.classList.remove('flex_column');
+      title.setAttribute('aria-expanded', false);
+
+      // 만약 로그인 한 사람이 본인이거나 어드민일 경우 다 보임
+    }
+  });
+}
+secretCheck();
 toggleContent(reviewTitle, reviewContents);
 toggleContent(inquiryTitle, inquiryContents);
