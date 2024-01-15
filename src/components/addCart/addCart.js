2d7c9a8587c190dc42620717aa83d20cad8a3a36
eddiekeem <cm730712@gmail.com>
2024-01-16 00:51:11 +0900

    [✨Feat]: addCart modal UI 구현

--- src/components/addCart/addCart.js
+++ src/components/addCart/addCart.js
@@ -1,21 +1,12 @@
-const addCartModal = document.querySelector('#addCartModal');
-const closeModalBtn = document.querySelector('#closeModal');
-
-const addBtn = document.querySelector('#addBtn');
-
 // 클릭했을 때 모달 열리기
-function openModal() {
-  console.log('열려라 모달');
-  addCartModal.classList.remove('modal_hidden');
-}
+export const openModal = (element, className) => {
+  element.classList.remove(className);
+};
 
 // 취소 버튼 클릭하여 모달 닫기
-function closeModal() {
-  console.log('닫혀라 모달');
-  addCartModal.classList.add('modal_hidden');
-}
-
-closeModalBtn.addEventListener('click', closeModal);
+export const closeModal = (element, className) => {
+  element.classList.add(className);
+};
 
 /* -------------------------------------------------------------------------- */
 /*                               상품 수량 증가, 감소 버튼                              */
